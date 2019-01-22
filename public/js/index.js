5f996d6ab0e80e5543337c119d556766c2533fea
Mohammed Farid <moha.khamis@outlook.com>
2019-01-22 10:09:32 +0200

    Scroll to bottom if user is near the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+    // selectors
+    var messages = $("#messages")
+    var newMessage = messages.children('li:last-child');
+
+    // height  
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if( clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 })
@@ -8,14 +25,14 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-$('#message-form').on('submit', function(e) {
+$('#message-form').on('submit', function (e) {
     e.preventDefault();
 
     var messageTextBox = $('[name=message]')
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextBox.val()
-    }, function() {
+    }, function () {
         messageTextBox.val('');
     })
 
@@ -23,7 +40,7 @@ $('#message-form').on('submit', function(e) {
 
 socket.on('newMessage', function (message) {
 
-    if(message.text.trim() === ''){
+    if (message.text.trim() === '') {
         message.text = "I'm stupid that I forgot to use a text"
     }
 
@@ -36,9 +53,10 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 })
 
-socket.on('newLocationMessage', function(message) {
+socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = $('#location-message-template').html();
     var html = Mustache.render(template, {
@@ -48,12 +66,13 @@ socket.on('newLocationMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 var locationBtn = $('#send-location');
 
 locationBtn.on('click', function () {
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
 
